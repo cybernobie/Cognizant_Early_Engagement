ae7a1d05754daf8f23cbe9346561be5985deef63
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-09-15 23:36:02 +0530

    Solve Fuel Consuption

--- Dashboard/Java Programming Funcamentals and Soft Skills/Java Programming Language/Fuel Consumption Calculator/Main.java
+++ Dashboard/Java Programming Funcamentals and Soft Skills/Java Programming Language/Fuel Consumption Calculator/Main.java
@@ -2,55 +2,33 @@ import java.util.*;
 import java.text.*;
 
 class Main {
+    private static void calculateFuelConsumption(double liter, double km) {
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        double gallon = liter * 0.2642;
+        double mile = km * 0.6214;
+
+        double literPerKm  = (liter / km) * 100;
+        double gallonPerKm = mile / gallon;
+
+        System.out.println("Liters/100KM");
+        System.out.println(decimalFormat.format(literPerKm));
+        System.out.println("Miles/gallons");
+        System.out.println(decimalFormat.format(gallonPerKm));
+    }
+    private static void test(double value) {
+        if (value <= 0) {
+            System.out.println((int) value + " is an Invalid Input");
+            System.exit(0);
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the no of ticket");
-        double tickets = scanner.nextDouble();
-
-        if (tickets < 5 || tickets > 40) {
-            System.out.println("Minimum of 5 and Maximum of 40 Tickets");
-            System.exit(0);
-        }
-
-        System.out.println("Do you want refreshment:");
-        boolean needRefreshment = scanner.next().charAt(0) == 'y';
-
-        System.out.println("Do you have coupon code:");
-        boolean haveCoupon = scanner.next().charAt(0) == 'y';
-
-        System.out.println("Enter the circle:");
-        char circle = scanner.next().charAt(0);
-
-        if (circle != 'k' && circle != 'q') {
-            System.out.println("Invalid Input");
-            System.exit(0);
-        }
-
-        double ticketCost;
-
-        if (circle == 'k') {
-            ticketCost = tickets * 75.0;
-        } else {
-            ticketCost = tickets * 150.0;
-        }
-
-        double totalCost = ticketCost;
-
-        if (tickets > 20) {
-            double discount = totalCost * 10 / 100;
-            totalCost -= discount;
-        }
-
-        if (haveCoupon) {
-            double discount = totalCost * 2 / 100;
-            totalCost -= discount;
-        }
-
-        if (needRefreshment) {
-            totalCost += tickets * 50.0;
-        }
-
-        System.out.println("Ticket cost:" + new DecimalFormat("0.00").format(totalCost));
+        System.out.println("Enter the no of liters to fill the tank");
+        double liter = scanner.nextDouble();
+        test(liter);
+        System.out.println("Enter the distance covered");
+        double km = scanner.nextDouble();
+        test(km);
+        calculateFuelConsumption(liter, km);
     }
 }
