acca14ada602bc516af641d758266040f52ddfdc
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-09-16 13:20:54 +0530

    Solve Least Offer

---
+++ Dashboard/Java Programming Funcamentals and Soft Skills/Java Programming Language/Least Offer/Main.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Product implements Comparable<Product> {
+    private final String name;
+    private final double price;
+    private final double discount;
+
+    public Product(String name, double price, double discount) {
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+    }
+
+    public Double getDiscountPrice() {
+        return price * discount / 100;
+    }
+
+    @Override
+    public int compareTo(Product other) {
+        return this.getDiscountPrice().compareTo(other.getDiscountPrice());
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Product> products = new ArrayList<>();
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; ++i) {
+            String[] inputs = scanner.next().split(",");
+            String name = inputs[0];
+            double price = Double.parseDouble(inputs[1]);
+            double discount = Double.parseDouble(inputs[2]);
+
+            Product product = new Product(name, price, discount);
+            products.add(product);
+        }
+
+        double minimumDiscount = Collections.min(products);
+
+        for (Product product : products) {
+            if (product.getDiscountPrice() == minimumDiscount) {
+                System.out.println(product);
+            }
+        }
+    }
+}
