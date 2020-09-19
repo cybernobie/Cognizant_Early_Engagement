d23e2412e0c5c204f79fd3365e2403c8ad780f25
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-09-19 13:15:43 +0530

    Fix Password Generation

--- Dashboard/Java Programming Funcamentals and Soft Skills/Java Programming Language/Password Generation/Main.java
+++ Dashboard/Java Programming Funcamentals and Soft Skills/Java Programming Language/Password Generation/Main.java
@@ -1,4 +1,3 @@
-import java.util.Random;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -47,11 +46,24 @@ class Main {
         return count >= 1;
     }
 
+    private static boolean checkOneOrMoreDigit(String code) {
+        char[] chars = code.toCharArray();
+        int count = 0;
+
+        for (char ch : chars) {
+            if (Character.isDigit(ch)) {
+                ++count;
+            }
+        }
+
+        return count >= 1;
+    }
+
     private static boolean checkValidity(String code) {
         char[] chars = code.toCharArray();
 
         for (char ch : chars) {
-            if (!Character.isUpperCase(ch) && !Character.isLowerCase(ch) && !Character.isLetterOrDigit(ch) && !Character.isDigit(ch) && ch != '@' && ch != '*' && ch != '#') {
+            if (!Character.isUpperCase(ch) && !Character.isLowerCase(ch) && !Character.isLetterOrDigit(ch) && ch != '@' && ch != '*' && ch != '#') {
                 return false;
             }
         }
@@ -61,32 +73,41 @@ class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Pattern pattern = Pattern.compile("^(?=(?:[^A-Z]*[A-Z]+[^A-Z]*))(?=(?:[^a-z]*[a-z]+[^a-z]*))(?=(?:[^@*#]*[@*#]+[^@*#]*))[a-zA-Z\\d@*#]{8,}");
+        Pattern pattern = Pattern.compile("^(?=(?:[^A-Z]*[A-Z]+[^A-Z]*))(?=(?:[^a-z]*[a-z]+[^a-z]*))(?=(?:[^@*#]*[@*#]+[^@*#]*))(?=(?:\\D*\\d+\\D*))[a-zA-Z\\d@*#]{8,}");
 
         System.out.println("Generate your Security Code");
         String code = scanner.nextLine();
         Matcher matcher = pattern.matcher(code);
 
-        boolean b1 = checkSize(code);
-        boolean b2 = checkMoreThanOneUpperCase(code);
-        boolean b3 = checkOneOrMoreLowerCase(code);
-        boolean b4 = checkOneOrMoreSpecialCharacter(code);
-        boolean b5 = checkValidity(code);
-
-        int random = new Random().nextInt(2);
-
-        if (matcher.matches() && b1 && b2 && b3 && b4 && b5) {
-            // This is just a little cheating
-            // Don't judge me
-            // The instructions weren't clear
-            // And I couldn't get the 3 case done
-            if (random == 0) {
-                System.out.println("Security Code Generated Successfully");
-            } else {
-                System.out.println("Invalid Security Code, Try Again!");
-            }
+        if (matcher.matches()) {
+            System.out.println("Security Code Generated Successfully");
         } else {
             System.out.println("Invalid Security Code, Try Again!");
         }
+
+//        boolean b1 = checkSize(code);
+//        boolean b2 = checkMoreThanOneUpperCase(code);
+//        boolean b3 = checkOneOrMoreLowerCase(code);
+//        boolean b4 = checkOneOrMoreSpecialCharacter(code);
+//        boolean b5 = checkValidity(code);
+//        boolean b6 = checkOneOrMoreDigit(code);
+//
+//        if (b1 && b2 && b3 && b4 && b5 && b6) {
+//            System.out.println("Security Code Generated Successfully");
+//        } else {
+//            System.out.println("Invalid Security Code, Try Again!");
+//        }
+
+//        Pattern pattern = Pattern.compile("((?=.*[A-Z])(?=.*[a-z])(?=.*[@#*])(?=.*\\d))");
+//
+//        System.out.println("Generate your Security Code");
+//        String code = scanner.nextLine();
+//        Matcher matcher = pattern.matcher(code);
+//
+//        if (matcher.find() && code.length() >= 8) {
+//            System.out.println("Security Code Generated Successfully");
+//        } else {
+//            System.out.println("Invalid Security Code, Try Again!");
+//        }
     }
 }
