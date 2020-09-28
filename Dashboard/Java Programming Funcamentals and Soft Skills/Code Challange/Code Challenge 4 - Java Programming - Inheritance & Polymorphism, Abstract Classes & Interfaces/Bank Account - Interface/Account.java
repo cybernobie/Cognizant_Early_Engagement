3d8da07e3a7d198f77c3a44a06e7a94ad4b74789
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-09-28 21:22:34 +0530

    Solve Bank Account - Interface

---
+++ Dashboard/Java Programming Funcamentals and Soft Skills/Code Challange/Code Challenge 4 - Java Programming - Inheritance & Polymorphism, Abstract Classes & Interfaces/Bank Account - Interface/Account.java
@@ -0,0 +1,35 @@
+public class Account {
+    private String accountNumber;
+    private String customerName;
+    private double balance;
+
+    public Account(String accountNumber, String customerName, double balance) {
+        this.accountNumber = accountNumber;
+        this.customerName = customerName;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
