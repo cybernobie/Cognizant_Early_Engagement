64ab61c0fe21dc2d2bb04b1784cf39e5a192e652
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-10-01 08:31:33 +0530

    Add another solution to Number Palindrome

--- Dashboard/Java Programming Funcamentals and Soft Skills/Java Programming Language/Number Palindrome/Palindrome.java
+++ Dashboard/Java Programming Funcamentals and Soft Skills/Java Programming Language/Number Palindrome/Palindrome.java
@@ -1,6 +1,25 @@
 import java.util.Scanner;
 
 public class Palindrome {
+    // private static boolean isPalindrome(int num) {
+    //     String numString = String.valueOf(num);
+    //     String reverse = new StringBuffer(numString).reverse().toString();
+    //     return reverse.equals(numString);
+    // }
+
+    private static boolean isPalindrome(int num) {
+        String numString = String.valueOf(num);
+        int size = numString.length();
+
+        for (int i = 0; i < size / 2; ++i) {
+            if (numString.charAt(i) != numString.charAt(size - i -1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
@@ -8,10 +27,7 @@ public class Palindrome {
         if (num < 0) {
             System.out.println("Invalid Input");
         } else {
-            String numString = String.valueOf(num);
-            String reverse = new StringBuffer(numString).reverse().toString();
-
-            if (reverse.equals(numString)) {
+            if (isPalindrome1(num)) {
                 System.out.println("Palindrome");
             } else {
                 System.out.println("Not a Palindrome");
