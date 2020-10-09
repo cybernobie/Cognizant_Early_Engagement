27ca54dfd5ba5e24f5e92cde827c30d28aebad6f
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-10-09 16:24:01 +0530

    Fix the main method

--- Dashboard/Java Programming Funcamentals and Soft Skills/Java Programming Language/Least Offer/Main.java
+++ Dashboard/Java Programming Funcamentals and Soft Skills/Java Programming Language/Least Offer/Main.java
@@ -24,8 +24,6 @@ class Product implements Comparable<Product> {
     public String toString() {
         return name;
     }
-
-    
 }
 
 class Main {
@@ -44,6 +42,8 @@ class Main {
             products.add(product);
         }
 
-        
+        products.stream()
+                .filter((product) -> product.getDiscountPrice().equals(Collections.min(products).getDiscountPrice()))
+                .forEach(System.out::println);
     }
 }
